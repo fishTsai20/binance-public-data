3461ed72dcc117f69721a53694b18ce68b8ad8d8
fishTsai20 <isaisfish@gmail.com>
2024-12-10 11:54:48 +0800

    fix[download]:if file url not exist, won't retry

--- python/utility.py
+++ python/utility.py
@@ -32,8 +32,49 @@ def get_all_symbols(type):
 
 def download_file_with_retry(max_retries, base_path, file_name, date_range=None, folder=None):
     for attempt in range(1, max_retries + 1):
-        if download_file(base_path, file_name, date_range, folder):
+        download_path = "{}{}".format(base_path, file_name)
+        if folder:
+            base_path = os.path.join(folder, base_path)
+        if date_range:
+            date_range = date_range.replace(" ", "_")
+            base_path = os.path.join(base_path, date_range)
+        save_path = get_destination_dir(os.path.join(base_path, file_name), folder)
+
+        if os.path.exists(save_path):
+            print("\nfile already exists! {}".format(save_path))
             return True
+
+        # make the directory
+        if not os.path.exists(base_path):
+            Path(get_destination_dir(base_path)).mkdir(parents=True, exist_ok=True)
+
+        try:
+            download_url = get_download_url(download_path)
+            dl_file = urllib.request.urlopen(download_url)
+            length = dl_file.getheader('content-length')
+            if length:
+                length = int(length)
+                blocksize = max(4096, length // 100)
+
+            with open(save_path, 'wb') as out_file:
+                dl_progress = 0
+                print("\nFile Download: {}".format(save_path))
+                while True:
+                    buf = dl_file.read(blocksize)
+                    if not buf:
+                        break
+                    dl_progress += len(buf)
+                    out_file.write(buf)
+                    done = int(50 * dl_progress / length)
+                    sys.stdout.write("\r[%s%s]" % ('#' * done, '.' * (50 - done)))
+                    sys.stdout.flush()
+            return True
+        except urllib.error.HTTPError:
+            print("\nFile not found: {}".format(download_url))
+            return False
+        except Exception as e:
+            print("\ndownload err: {}".format(e))
+            continue
     return False
 
 
